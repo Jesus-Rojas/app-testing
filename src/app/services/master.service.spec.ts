7d7083cf09d0ffefc09ce7bd9455c52ac4f220c1
Jesus Rojas <jesus@invorious.com>
2024-01-12 17:12:16 -0500

    feat: add spies in MasterService :test_tube:

--- src/app/services/master.service.spec.ts
+++ src/app/services/master.service.spec.ts
@@ -22,4 +22,13 @@ fdescribe('MasterService', () => {
     const masterService = new MasterService(fakeObject as ValueService);
     expect(masterService.getValue()).toBe('fake value');
   });
+
+  it('should call to getValue from ValueService', () => {
+    const valueServiceSpy = jasmine.createSpyObj('ValueService', ['getValue']);
+    valueServiceSpy.getValue.and.returnValue('fake value');
+    const masterService = new MasterService(valueServiceSpy);
+    expect(masterService.getValue()).toBe('fake value');
+    expect(valueServiceSpy.getValue).toHaveBeenCalled();
+    expect(valueServiceSpy.getValue).toHaveBeenCalledTimes(1);
+  });
 });
