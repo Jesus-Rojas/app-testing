fcfeafbbeb9eea96758644acca8ddb07c3cca514
Jesus Rojas <jesus@invorious.com>
2024-01-12 16:44:45 -0500

    feat: add unit testing to ValueService :sparkles:

--- src/app/services/value.service.spec.ts
+++ src/app/services/value.service.spec.ts
@@ -1,16 +1,58 @@
-import { TestBed } from '@angular/core/testing';
+// import { TestBed } from '@angular/core/testing';
 
+import { firstValueFrom } from 'rxjs';
 import { ValueService } from './value.service';
 
-describe('ValueService', () => {
+fdescribe('ValueService', () => {
   let service: ValueService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ValueService);
+    service = new ValueService();
   });
 
-  it('should be created', () => {
+  it('should be create', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('Tests for getValue', () => {
+    it('should return "my value"', () => {
+      expect(service.getValue()).toBe('my value');
+    });
+  });
+
+  describe('Tests for setValue', () => {
+    it('should change the value', () => {
+      expect(service.getValue()).toBe('my value');
+      service.setValue('change')
+      expect(service.getValue()).toBe('change');
+    });
+  });
+
+  describe('Tests for getPromiseValue', () => {
+    it('should return "promise value" from promise with then', (doneFn) => {
+      service.getPromiseValue().then((value) => {
+        expect(value).toBe('promise value');
+        doneFn();
+      });
+    });
+
+    it('should return "promise value" from promise with async', async () => {
+      const value = await service.getPromiseValue();
+      expect(value).toBe('promise value');
+    });
+  });
+
+  describe('Tests for getObservableValue', () => {
+    it('should return "observable value" from observable with subscribe', (doneFn) => {
+      service.getObservableValue().subscribe((value) => {
+        expect(value).toBe('observable value');
+        doneFn();
+      });
+    });
+
+    it('should return "observable value" from observable with async', async () => {
+      const value = await firstValueFrom(service.getObservableValue());
+      expect(value).toBe('observable value');
+    });
   });
 });
