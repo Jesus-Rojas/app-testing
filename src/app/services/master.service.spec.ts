e0f5e9bc8f7c2b6dec133dab26a8dd7bd20021aa
Jesus Rojas <jesus@invorious.com>
2024-01-12 17:02:39 -0500

    feat: add mocks in MasterService :test_tube:

---
+++ src/app/services/master.service.spec.ts
@@ -0,0 +1,25 @@
+// import { TestBed } from '@angular/core/testing';
+
+import { FakeValueService } from './fake-value.service';
+import { MasterService } from './master.service';
+import { ValueService } from './value.service';
+
+fdescribe('MasterService', () => {
+  it('should return "my value" from the real service', () => {
+    const valueService = new ValueService();
+    const masterService = new MasterService(valueService);
+    expect(masterService.getValue()).toBe('my value');
+  });
+
+  it('should return "my value" from the fake service', () => {
+    const fakeValueService = new FakeValueService();
+    const masterService = new MasterService(fakeValueService as unknown as ValueService);
+    expect(masterService.getValue()).toBe('fake value');
+  });
+
+  it('should return "my value" from the fake object', () => {
+    const fakeObject = { getValue: () => 'fake value' };
+    const masterService = new MasterService(fakeObject as ValueService);
+    expect(masterService.getValue()).toBe('fake value');
+  });
+});
