5ebe033827843146be887dbc4f48862375b90cb9
Jesus Rojas <jesus@invorious.com>
2024-04-09 13:10:44 -0500

    feat: add test on components with dependencies :test_tube:

--- src/app/components/products/products.component.spec.ts
+++ src/app/components/products/products.component.spec.ts
@@ -1,23 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+
+import { generateManyProducts } from 'src/app/models/product.mock';
+import { ProductsService } from 'src/app/services/products.service';
+import { ProductComponent } from '../product/product.component';
 
 import { ProductsComponent } from './products.component';
 
-xdescribe('ProductsComponent', () => {
+fdescribe('ProductsComponent', () => {
   let component: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
+  let productsService: jasmine.SpyObj<ProductsService>;
 
   beforeEach(async () => {
+    const productsServiceSpy = jasmine.createSpyObj('ProductsService', ['getAll']);
+
     await TestBed.configureTestingModule({
-      declarations: [ ProductsComponent ]
+      declarations: [
+        ProductsComponent,
+        ProductComponent,
+      ],
+      providers: [
+        {
+          provide: ProductsService,
+          useValue: productsServiceSpy
+        },
+      ],
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(ProductsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    productsService = TestBed.inject(ProductsService) as jasmine.SpyObj<ProductsService>;
   });
 
   it('should create', () => {
+    const productsMock = generateManyProducts(3);
+    productsService.getAll.and.returnValue(of(productsMock));
+    fixture.detectChanges(); // ngOnInit
+
     expect(component).toBeTruthy();
+    expect(productsService.getAll).toHaveBeenCalled();
   });
 });
