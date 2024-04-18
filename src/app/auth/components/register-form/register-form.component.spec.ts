bc8db9a1811a799061a7c4307e0384b6743db886
Jesus Rojas <jesus@invorious.com>
2024-04-18 00:28:11 -0500

    fix: solve bugs in tests units :bug:

--- src/app/auth/components/register-form/register-form.component.spec.ts
+++ src/app/auth/components/register-form/register-form.component.spec.ts
@@ -1,19 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { UsersService } from '../../services/users.service';
 
 import { RegisterFormComponent } from './register-form.component';
 
 describe('RegisterFormComponent', () => {
   let component: RegisterFormComponent;
   let fixture: ComponentFixture<RegisterFormComponent>;
+  let usersService: jasmine.SpyObj<UsersService>;
 
   beforeEach(async () => {
+    const usersServiceSpy = jasmine.createSpyObj('UsersService', ['getAll']);
     await TestBed.configureTestingModule({
-      declarations: [ RegisterFormComponent ]
+      imports: [ ReactiveFormsModule ],
+      declarations: [ RegisterFormComponent ],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: usersServiceSpy
+        },
+      ],
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(RegisterFormComponent);
     component = fixture.componentInstance;
+    usersService = TestBed.inject(UsersService) as jasmine.SpyObj<UsersService>;
     fixture.detectChanges();
   });
 
