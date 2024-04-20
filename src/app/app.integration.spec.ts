37d6015c67c10df92bd0fc45dee18fddb50f0d5d
Jesus Rojas <jesus@invorious.com>
2024-04-19 23:48:42 -0500

    test: components with dependencies :test_tube:

--- src/app/app.integration.spec.ts
+++ src/app/app.integration.spec.ts
@@ -2,21 +2,33 @@ import { ComponentFixture, fakeAsync, TestBed, tick } from "@angular/core/testin
 import { Router, RouterLinkWithHref } from "@angular/router";
 import { RouterTestingModule } from "@angular/router/testing";
 
-import { clickElementById, query, queryAllByDirective } from "src/testing";
+import { clickElementById, getTextById, observableSuccess, query, queryAllByDirective } from "src/testing";
 import { AppComponent } from "./app.component";
 import { routes } from "./app-routing.module";
 import { AppModule } from "./app.module";
+import { ProductsService } from "./products/services/products.service";
+import { of } from "rxjs";
+import { generateManyProducts } from "./mocks/product.mock";
 
 fdescribe('App Integration Test', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
   let router: Router;
+  let productsService: jasmine.SpyObj<ProductsService>;
 
   beforeEach(fakeAsync(async () => {
+    const productsServiceSpy = jasmine.createSpyObj('ProductsService', ['getAll']);
+
     await TestBed.configureTestingModule({
       imports: [
         AppModule,
         RouterTestingModule.withRoutes(routes),
+      ],
+      providers: [
+        {
+          provide: ProductsService,
+          useValue: productsServiceSpy,
+        },
       ],
     }).compileComponents();
 
@@ -25,6 +37,7 @@ fdescribe('App Integration Test', () => {
     fixture.detectChanges();
 
     // providers
+    productsService = TestBed.inject(ProductsService) as jasmine.SpyObj<ProductsService>;
     router = TestBed.inject(Router);
     router.initialNavigation();
     tick();
@@ -41,11 +54,18 @@ fdescribe('App Integration Test', () => {
   });
 
   it('should render OthersComponent when clicked', fakeAsync(() => {
+    const productsMock = generateManyProducts(10);
+    productsService.getAll.and.returnValue(observableSuccess(productsMock));
+    
     clickElementById(fixture, 'others-link');
-    tick();
+    tick(); // wait while nav ...
     fixture.detectChanges();
+    tick(); // exec productsService.getAll()
+    fixture.detectChanges(); // update html
+
     expect(router.url).toEqual('/others');
     expect(query(fixture, 'app-others')).not.toBeNull();
+    expect(getTextById(fixture, 'products-length')).toContain(productsMock.length);
   }));
 
   it('should render PicoPreviewComponent when clicked', fakeAsync(() => {
--- src/app/components/others/others.component.ts
+++ src/app/components/others/others.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { ProductsService } from 'src/app/products/services/products.service';
 
 @Component({
   selector: 'app-others',
@@ -8,4 +10,13 @@ import { Component } from '@angular/core';
 export class OthersComponent {
   color = 'blue';
   text = 'roma';
+  products: Product[] = [];
+
+  constructor(
+    private productsService: ProductsService,
+  ) { }
+
+  ngOnInit(): void {
+    this.productsService.getAll().subscribe((products) => (this.products = products));
+  }
 }
